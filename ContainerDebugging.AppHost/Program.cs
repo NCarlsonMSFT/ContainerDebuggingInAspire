b2b7b221ffb5911a2c0abc604cd5eaa32e53229d
Nathan Carlson <ncarlson@microsoft.com>
2025-06-27 16:13:13 -0700

    Switch to using container launch profile

--- ContainerDebugging.AppHost/Program.cs
+++ ContainerDebugging.AppHost/Program.cs
@@ -1,39 +1,30 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 #if RUN_IN_CONTAINER
+string? apiServiceLaunchProfileName = "Container (.NET SDK)";
+string? webFrontEndLaunchProfileName = "Container (.NET SDK)";
 
-// Ensure vsdbg has been downloaded
-ContainerHelpers.DownloadVsdbg(Path.Combine(builder.AppHostDirectory, "vsdbg"));
-
-// Configure the Apire host to use a cert that handles host.docker.internal
+// Configure the Aspire host to use a cert that handles host.docker.internal
 (string certPath, string certPassword) = ContainerHelpers.EnsureGeneratedCertificate(builder);
 Environment.SetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Path", certPath);
 Environment.SetEnvironmentVariable("ASPNETCORE_Kestrel__Certificates__Default__Password", certPassword);
-
-var apiService = builder.AddDebuggableContainer(
-                            name: "apiservice",
-                            projectName: "ContainerDebugging.ApiService",
-                            image: "containerdebuggingapiservice",
-                            tag: "dev");
-    
-var webfrontend = builder.AddDebuggableContainer(
-                            name: "webfrontend",
-                            projectName: "ContainerDebugging.Web",
-                            image: "containerdebuggingweb",
-                            tag: "dev")
-                    .WithReference(apiService.GetEndpoint("http"))
-                    .WithReference(apiService.GetEndpoint("https"));
-
 #else
-
-var apiService = builder.AddProject<Projects.ContainerDebugging_ApiService>("apiservice");
-
-var webfrontend = builder.AddProject<Projects.ContainerDebugging_Web>("webfrontend")
-                         .WithReference(apiService);
-
+string? apiServiceLaunchProfileName = null;
+string? webFrontEndLaunchProfileName = null;
 #endif
 
-webfrontend.WithExternalHttpEndpoints()
-           .WaitFor(apiService);
+var apiService = builder.AddProject<Projects.ContainerDebugging_ApiService>("apiservice", apiServiceLaunchProfileName)
+#if RUN_IN_CONTAINER
+                    .WithDebuggableContainer(hostHttpPort: 5307, hostHttpsPort: 7492)
+#endif
+;
+
+var webfrontend = builder.AddProject<Projects.ContainerDebugging_Web>("webfrontend", webFrontEndLaunchProfileName)
+#if RUN_IN_CONTAINER
+                    .WithDebuggableContainer(hostHttpPort: 5192, hostHttpsPort: 7126)
+#endif
+                    .WithReference(apiService)
+                    .WithExternalHttpEndpoints()
+                    .WaitFor(apiService);
 
 builder.Build().Run();
--- ContainerDebugging.ServiceDefaults/ContainerHelpers.cs
+++ ContainerDebugging.ServiceDefaults/ContainerHelpers.cs
@@ -1,17 +1,25 @@
 ﻿#if RUN_IN_CONTAINER
-using System.Diagnostics;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace ContainerDebugging.ServiceDefaults
 {
-    public class ContainerHelpers
+    public partial class ContainerHelpers
     {
+        [GeneratedRegex("(/)localhost([:/])", RegexOptions.IgnoreCase, "en-US")]
+        private static partial Regex LocalhostRegex();
+
         public static void WaitForSetup()
         {
-            if (string.Equals(Environment.GetEnvironmentVariable("WAIT_FOR_DEBUGGING"), "true"))
+            Regex regex = LocalhostRegex();
+            foreach (DictionaryEntry entry in Environment.GetEnvironmentVariables())
             {
-                while(!Debugger.IsAttached)
+                string value = entry.Value as string ?? string.Empty;
+
+                if (value.Contains("localhost"))
                 {
-                    Thread.Sleep(100);
+                    string newValue = regex.Replace(value, "$1host.docker.internal$2");
+                    Environment.SetEnvironmentVariable((string)entry.Key, newValue);
                 }
             }
 
